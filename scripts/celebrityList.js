9098f4e6edeb892c80173c02b5b07b48fd4de83a
JavontaeH <hardeman.javontae@gmail.com>
2022-03-08 14:47:26 -0600

    semi functional selector, need to specify ids more

--- scripts/celebrityList.js
+++ scripts/celebrityList.js
@@ -4,14 +4,14 @@ import { cycleBgColor } from "./helper.js";
 const celebritiesHTMLInsert = (obj) => {
   const celebrityTarget = document.querySelector(".celebrityList");
   celebrityTarget.innerHTML += `
-  <div class = "singleCelebrity${cycleBgColor()}">
-    <header class="name"><h1>${obj.name}: ${obj.title}</h1></header>
-    <section class="bio"><h2>${obj.bio} <a href="${obj.image}"><img src="${
-    obj.image
-  }" alt="celebrity picture"></a></h2></section>
-    <footer class="lifespan"><h3>${obj.lifespan.birth} - ${
+  <div class = "singleCelebrity${cycleBgColor()}" >
+    <header class="name" id = "name">${obj.name}: ${obj.title}</header>
+    <section class="bio" id = "bio"><img src="${
+      obj.image
+    }" alt="celebrity picture"> ${obj.bio}</section>
+    <footer class="lifespan" id = "lifespan">${obj.lifespan.birth} - ${
     obj.lifespan.death
-  }</h3></footer>
+  }</footer>
   </div>
   `;
 };
@@ -21,3 +21,11 @@ export const buildCelebrities = (data) => {
     celebritiesHTMLInsert(celebrity);
   });
 };
+
+document
+  .querySelector(".celebrityList")
+  .addEventListener("click", (celebClickEvent) => {
+    console.log(celebClickEvent.target.id);
+    document.getElementById(`${celebClickEvent.target.id}`).style.borderStyle =
+      "dotted";
+  });
